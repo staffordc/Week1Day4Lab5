13369cd1db28b62411e5a97b2849a24f2ee8979c
staffordc <stafford.carh@gmail.com>
2018-07-19 13:39:53 -0400

    inital push for file creation

---
+++ Week1Day4Lab5/Week1Day4Lab5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Day4Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user for 1 to 10 int
+            Console.WriteLine("Numbers one to ten, pwease");
+            //Display the factorial of the number
+            Console.WriteLine({FactorialSet});
+            //Prompt for continue (y/n)
+            Console.WriteLine("Contin uWu ? yes or noooooo pwease");
+            
+            //Use a for loop
+            FactorialSet()
+            for
+            {
+            }
+            //long type (Parse.Long)
+        }
+    }
+}
