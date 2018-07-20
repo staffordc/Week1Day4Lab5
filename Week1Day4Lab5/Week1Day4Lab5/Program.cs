8c378128558e762d8607630b41da26709c08b3d4
staffordc <stafford.carh@gmail.com>
2018-07-19 21:38:51 -0400

    no bells or whistles, need to validate some items still

--- Week1Day4Lab5/Week1Day4Lab5/Program.cs
+++ Week1Day4Lab5/Week1Day4Lab5/Program.cs
@@ -8,21 +8,38 @@ namespace Week1Day4Lab5
 {
     class Program
     {
+        int userInt;
         static void Main(string[] args)
         {
             //Prompt user for 1 to 10 int
             Console.WriteLine("Numbers one to ten, pwease");
-            //Display the factorial of the number
-            Console.WriteLine({FactorialSet});
-            //Prompt for continue (y/n)
-            Console.WriteLine("Contin uWu ? yes or noooooo pwease");
-            
+            var UserInput = Console.ReadLine();         
+
             //Use a for loop
-            FactorialSet()
-            for
-            {
-            }
+            int.TryParse(UserInput, out int UserInt);
+
+            //Display the factorial of the number
+            Console.WriteLine(FactorialSet(UserInt));
+
+            //Prompt for continue (y/n)
+            Console.WriteLine("Contin uWu ? yiss or noooooo pwease");
+
+            Console.ReadLine();
             //long type (Parse.Long)
+        }
+        static long FactorialSet(int UserInt)
+        
+        {
+            long PrevProduct = 1;
+            
+            for (int i = 1; i <= UserInt; ++i)
+            {
+                PrevProduct = i * (PrevProduct);
+                
+            }
+            return PrevProduct;
+
+
         }
     }
 }
